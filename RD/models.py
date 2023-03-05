ee528d8273f47e62fea1636ac054a8cd8b84b9b8
Kshee04 <123535084+Kshee04@users.noreply.github.com>
2023-03-05 22:24:56 +0300

    Delete models.py

--- RD/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-#create models
-class Customer(models.Model):
-    user=models.OneToOneField(User, null=True, blank=True )
-    name=models.CharField(max_length=200, null=True)
-    email=models.CharField(max_length=200, null=True)
-
-    def __str__(self):__
