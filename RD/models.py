f00ce665c487c372af4b4bc75ad3045a66d15e0a
Kshee04 <123535084+Kshee04@users.noreply.github.com>
2023-03-05 22:29:43 +0300

    Update models.py

--- RD/models.py
+++ RD/models.py
@@ -2,8 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 #create models
 class Customer(models.Model):
-    user=models.OneToOneField(User, null=True, blank=True )
+    user=models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True )
     name=models.CharField(max_length=200, null=True)
     email=models.CharField(max_length=200, null=True)
 
-    def __str__(self):__
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name=models.CharField(max_length=200, null=True)
+    price=models.FloatField()
+    digital= models.BooleanField(default=False, null=True, blank=False)
+    # image
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer=models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_ordered=models.DateTimeField(auto_now_add=True)
+    complete=models.BooleanField(default=False, null=True, blank=False)
+    name = models.CharField(max_length=200, null=True)
+    transaction_id=models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
